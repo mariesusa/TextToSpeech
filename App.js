de096f0d04841d8361d45735301ae5bb33281380
mariesusa <mari.remmler@hotmail.com>
2022-10-08 09:12:34 +0300

    https://github.com/mariesusa/TextToSpeech.git

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+const [text, setText] = useState('');
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <TextInput
+        style={ styles.input }
+        keyboardType='default'
+        onChangeText={ text => setText(text) }
+        value={ text }
+        placeholder='Text'
+      />
+
+      <View>
+        <Button color='#00cccc' 
+          title='PRESS TO HEAR TEXT'
+          onPress={ speak }
+        />
+      </View>
+
     </View>
   );
 }
@@ -16,5 +39,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 10,
+    marginTop: 20
+  },
+  input : {
+    width: '80%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 3,
+    marginTop: 3,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  button : {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'lightblue',
+    margin: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '20%',
+    height: 40
   },
 });
